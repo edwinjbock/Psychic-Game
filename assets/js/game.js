9e3585a6c77078e28c7bc6163c3725e9536b0e5f
Ed Bock <edwinjbock@gmail.com>
2018-09-07 00:12:08 -0400

    going to bed

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,5 @@
 // List all of the array possibilities
-var computerChoices = ["a", "b", "c"];
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // Variables to hold the data inputted so far
 var wins = 0;
@@ -12,29 +12,34 @@ var lossesText = document.getElementById("htmlLosses");
 var guessesRemainingText = document.getElementById("htmlGuessesRemaining");
 var guessesSoFarText = document.getElementById("htmlGuessesSoFar");
 
-// Use these as needed for testing links
+// ******* TESTING ********
 // console.log(winsText);
 // console.log(lossesText);
 // console.log(guessesRemainingText);
 // console.log(guessesSoFarText);
 
-// Kick off the game with a keystroke
+// KICK OFF THE GAME WITH A KEYSTROKE
 document.onkeyup = function (event) {
 
   // Clears the You Won/You Lost message after a key press
   var resultsText = document.getElementById("htmlMessage");
 
-  // Determines which key was pressed.
+  // Vet the key stroke, making sure that it is contained in the array above
+  // <A>
+
+  // Determine which key was pressed and then convert it to lowercase for comparing to the array above
   var userGuess = event.key;
+  var userGuessVetted = userGuess.toLowerCase();
 
   // Randomly choose a choice from the options array. This is the Computer's guess.
   // <A> FIND A RANDOMIZER!!!!!!!!!!!!
   var computerChoice = "a";
 
-// Use these as needed for testing 
-// console.log("userGuess: " + userGuess);
-// console.log("computerChoice: " + computerChoice);
-  
+// ******* TESTING ********
+console.log("userGuess: " + userGuess);
+console.log("computerChoice: " + computerChoice);
+console.log("userGuessVetted: " + userGuessVetted);
+
   
   
   
@@ -44,7 +49,7 @@ document.onkeyup = function (event) {
   // <A> Need logic for when it reaches zero
   guessesRemainingText.textContent = guessesRemaining - 1;
 
-  
+
 
 
 
