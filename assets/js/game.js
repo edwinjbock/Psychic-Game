5dbfd4ef9e340bc9dfb843eb33a75fd7c56fd170
Ed Bock <edwinjbock@gmail.com>
2018-09-07 12:01:08 -0400

    update javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,6 +11,7 @@ var winsText = document.getElementById("htmlWins");
 var lossesText = document.getElementById("htmlLosses");
 var guessesRemainingText = document.getElementById("htmlGuessesRemaining");
 var guessesSoFarText = document.getElementById("htmlGuessesSoFar");
+var resultsText = document.getElementById("htmlMessage");
 
 // ******* TESTING ********
 // console.log(winsText);
@@ -21,41 +22,79 @@ var guessesSoFarText = document.getElementById("htmlGuessesSoFar");
 // KICK OFF THE GAME WITH A KEYSTROKE
 document.onkeyup = function (event) {
 
-  // Clears the You Won/You Lost message after a key press
-  var resultsText = document.getElementById("htmlMessage");
-
-  // Vet the key stroke, making sure that it is contained in the array above
-  // <A>
+  // Clears the You Won/You Lost message after a key is pressed
+  var resultsText = "";
 
   // Determine which key was pressed and then convert it to lowercase for comparing to the array above
   var userGuess = event.key;
   var userGuessVetted = userGuess.toLowerCase();
 
-  // Randomly choose a choice from the options array. This is the Computer's guess.
-  // <A> FIND A RANDOMIZER!!!!!!!!!!!!
-  var computerChoice = "a";
+  // Computer Choice
+  If ()
+  var computerChoice = computerChoices[Math.floor(Math.random() * (computerChoices.length - 1))];
+  // Equate the result to a letter in the array
+  // ******* TESTING ********
+  console.log("computerChoices.length: " + computerChoices.length);
+  console.log("computerChoice: " + computerChoice);
+  console.log("computerChoices.length - 1: " + (computerChoices.length - 1));
 
-// ******* TESTING ********
-console.log("userGuess: " + userGuess);
-console.log("computerChoice: " + computerChoice);
-console.log("userGuessVetted: " + userGuessVetted);
+  // Make sure that the computer didn't randomly choose index zero, resulting in a possible value of index [-1]
 
-  
-  
-  
-  winsText.textContent = "wins";
-  lossesText.textContent = "losses";
 
-  // <A> Need logic for when it reaches zero
+
+  // Verify that the user pressed an alpha character
+  isAlpha(userGuess);
+
+  function isAlpha(str) {
+    if (!(str > 64 && str < 91) && // upper alpha (A-Z)
+      !(str > 96 && str < 123))  // lower alpha (a-z)
+        {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  // ******* TESTING ********
+  console.log("isAlpha: " + userGuess.isAlpha(userGuess));
+
+  // Vet the key stroke, making sure that it is contained in the array above
+  // <A>
+
+
+
+
+
+
+  // ******* TESTING ********
+  console.log("userGuess: " + userGuess);
+  console.log("computerChoice: " + computerChoice);
+  console.log("userGuessVetted: " + userGuessVetted);
+
+  // TESTING BELOW OUTPUT
+  winsText = 100;
+  lossesText = 101;
+  guessesRemainingText = 102;
+  guessesSoFarText = "a";
+  var resultsText = "Test";
+
+  // Port the results back to the DOM
+  htmlWins.textContent = winsText;
+  htmlLosses.textContent = lossesText;
+  htmlGuessesRemaining.textContent = guessesRemainingText;
+  htmlGuessesSoFar = htmlGuessesSoFar + ", " + guessesSoFarText;
+  htmlMessage = resultsText;
+
+
+  // <A> Need logic for when remaining guesses reach zero
   guessesRemainingText.textContent = guessesRemaining - 1;
 
 
 
 
 
-  
-  
-  
-  } // End of document.onkeyup
 
-  
+
+
+ // End of document.onkeyup
+
